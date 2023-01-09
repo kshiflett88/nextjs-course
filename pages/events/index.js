4293098c27da96e77f8d54a1d1e7dcc871948a93
kshiflett88 <kodishiflett@gmail.com>
2023-01-09 15:20:16 +0300

    add filter functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,22 @@
 import React, { Fragment } from 'react'
+import { useRouter } from 'next/router';
 import EvenList from '../../components/events/EvenList';
 import EventSearch from '../../components/events/EventSearch';
 import { getAllEvents } from '../../dummy-data';
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    
+    router.push(fullPath);
+  };
 
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EvenList items={events} />
     </Fragment>
   );
